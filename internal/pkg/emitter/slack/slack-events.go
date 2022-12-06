6ff10d1cc21c6f3a2986a556bb7f02beda1f614d
veezex-drewl <93376983+veezex-drewl@users.noreply.github.com>
2022-12-06 23:08:00 +0700

    fix

--- internal/pkg/emitter/slack/slack-events.go
+++ internal/pkg/emitter/slack/slack-events.go
@@ -36,39 +36,54 @@ func New(secret string, opts SlackEventsOps) *slack {
 func (s *slack) Listen(ctx context.Context) (<-chan message.Message, error) {
 	result := make(chan message.Message, 10)
 
+	finish := func(err error) {
+		result <- message.Message{
+			Author: "",
+			Text:   "",
+			Err:    err,
+		}
+		close(result)
+	}
+
 	http.HandleFunc(s.opts.Path, func(w http.ResponseWriter, r *http.Request) {
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			finish(err)
 			return
 		}
 		sv, err := slackContrib.NewSecretsVerifier(r.Header, s.secret)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			finish(err)
 			return
 		}
 		if _, err := sv.Write(body); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			finish(err)
 			return
 		}
 		if err := sv.Ensure(); err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
+			finish(err)
 			return
 		}
 		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			finish(err)
 			return
 		}
 
 		zeroLog.Info().Fields(eventsAPIEvent)
 	})
 
-	// run server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", s.opts.Port), nil)
-	if err != nil {
-		return nil, err
-	}
+	go func() {
+		err := http.ListenAndServe(fmt.Sprintf(":%d", s.opts.Port), nil)
+		if err != nil {
+			finish(err)
+		}
+	}()
 
 	return result, nil
 }
